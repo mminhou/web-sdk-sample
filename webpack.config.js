288d733bad73ac088c806ff14e0ecda5768945c3
mminhou <exit19093@gmail.com>
2022-04-12 10:44:05 +0900

    Initial: web-sdk-sample project 초기셋팅(using reactjs)

---
+++ webpack.config.js
@@ -0,0 +1,46 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.tsx',
+  output: {
+    filename: 'bundle.js',
+    publicPath: '/',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx|ts|tsx)$/,
+        use: [
+          'babel-loader',
+          {
+            loader: 'ts-loader',
+            options: {
+              transpileOnly: true,
+            },
+          },
+        ],
+        exclude: /node_modules/,
+      }
+    ],
+  },
+  resolve: {
+    extensions: ['*', '.js', '.jsx', '.ts', '.tsx'],
+    fallback: {
+      "zlib": false
+    } 
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, './public/index.html'),
+      inject: true,
+      filename: path.resolve(__dirname, './dist/index.html'),
+    }),
+  ],
+  mode: 'development',
+  devServer: {
+    port: 9000,
+    hot: true,
+  },
+}
