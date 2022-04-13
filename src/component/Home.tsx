b7096b01a12756684fa32f41033062940216a8b2
mminhou <exit19093@gmail.com>
2022-04-13 16:06:29 +0900

    Feat: Home component 구성 및 ConnectButton, PenBasedRenderer 렌더링 작업

---
+++ src/component/Home.tsx
@@ -0,0 +1,35 @@
+import { AppBar,makeStyles, Typography } from '@material-ui/core';
+import React from 'react';
+import ConnectButton from '../buttons/ConnectButton';
+import PenBasedRenderer from '../renderer/PenBasedRenderer';
+
+const useStyle = makeStyles((theme) => ({
+  mainBackground: {
+    width: '100%',
+    height: '100%',
+    display: 'flex',
+    flexDirection: 'column',
+    textAlign: 'center',
+  },
+  title: {
+    margin: '15px',
+  }
+}));
+
+const Home = () => {
+  const classes = useStyle();
+  
+  return (
+    <div className={classes.mainBackground}>
+      <AppBar position='relative' color='transparent' elevation={0}>
+        <Typography variant="h5" className={classes.title}>
+          WEB SDK SAMPLE
+        </Typography>
+      </AppBar>
+      <ConnectButton />
+      <PenBasedRenderer />
+    </div>
+  );
+};
+
+export default Home;
