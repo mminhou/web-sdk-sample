79aa4d7515598cb01b4b8567f0a8cd8e5511ccdb
mminhou <exit19093@gmail.com>
2022-04-21 10:07:39 +0900

    Feat: type 추가 선언

--- src/utils/type.ts
+++ src/utils/type.ts
@@ -5,12 +5,50 @@ type PageInfo = {
   page: number;
 };
 
+type PageInfo2 = {
+  section: number;
+  owner: number;
+  note: number;
+  page: number;
+}
+
 type PaperBase = {
   Xmin: number;
   Ymin: number;
 }
 
+type Dot = {
+  angle: object;
+  color: number;
+  dotType: number;
+  f: number;
+  pageInfo: PageInfo;
+  penTipType: number;
+  timestamp: number;
+  x: number;
+  y: number;
+}
+
+type PdfDot = {
+  x: number;
+  y: number;
+}
+
+type View = {
+  width: number;
+  height: number;
+}
+
+type Options = {
+  filters: any;
+}
+
 export type { 
   PageInfo,
+  PageInfo2,
   PaperBase,
+  Dot,
+  PdfDot,
+  View,
+  Options,
 };
